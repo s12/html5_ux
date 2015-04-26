a3fe51349f392f742651e6e0fd6713bc89ea9063
Michael Hurley <studio12designs@me.com>
2015-04-26 07:26:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# HTML5 User Experience
+## Extends Drupal Front End by adding vital javascript and css
+
+* Specifically:
+- modernizr.js (with respond/print-shiv)
+- jQuery Migrate
+- fix.css/fix.js
+-- adds general fixes to make sites more cross-platform compatible
+--- @see: http://getbootstrap.com/getting-started/#support-ie10-width
+--- @see: http://getbootstrap.com/getting-started/#support-android-stock-browser
---
+++ js/whitefox.js
@@ -0,0 +1,12 @@
+(function($){
+  $(document).ready(function(){
+    $('body.admin-menu [action*="/admin"] .form-actions [type=submit], body.admin-menu [action*="/edit"] .form-actions [type=submit], body.admin-menu [action*="/node/add"] .form-actions [type=submit], body.admin-menu #branding .tabs > li > a, body.admin-menu #page .tabs > li > a, body.admin-menu .region-content > .tabs > ul > li > a').each(function(i,o){
+
+      $(o).clone().appendTo('.whitefox-menu').click(function(e){
+        $(o).click();
+      });
+      
+      $(o).hide();
+    });    
+  });
+})(jQuery);
