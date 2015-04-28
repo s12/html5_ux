f3027f5437293ead71192d7b8c572f00b3a3e0e1
Michael Hurley <studio12designs@me.com>
2015-04-28 11:33:17 -0400

    views compete issues fixed

--- js/whitefox.js
+++ js/whitefox.js
@@ -1,12 +1,14 @@
 (function($){
   $(document).ready(function(){
-    $('body.admin-menu [action*="/admin"] .form-actions [type=submit], body.admin-menu [action*="/edit"] .form-actions [type=submit], body.admin-menu [action*="/node/add"] .form-actions [type=submit], body.admin-menu #branding .tabs > li > a, body.admin-menu #page .tabs > li > a, body.admin-menu .region-content > .tabs > ul > li > a').each(function(i,o){
+    $('body.admin-menu [action*="/admin"] .form-actions [type=submit], body.admin-menu [action*="/edit"] .form-actions [type=submit], body.admin-menu [action*="/node/add"] .form-actions [type=submit], body.admin-menu #branding .tabs > li > a, body.admin-menu #page .tabs > li > a:not(.add), body.admin-menu .region-content > .tabs > ul > li > a:not(.add)').each(function(i,o){
 
       $(o).clone().appendTo('.whitefox-menu').click(function(e){
         $(o).click();
       });
       
       $(o).hide();
+      
+      $("#views-display-menu-tabs > li > a").show();
     });    
   });
 })(jQuery);
